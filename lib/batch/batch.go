4e7832eaf66cbd9e6da50fd038e4d55bfb6cd6b6
Anastasiia_Dremina <anastasyadremina@rambler.ru>
2022-07-16 21:49:09 +0200

    solution

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -13,6 +13,32 @@ func getOne(id int64) user {
 	return user{ID: id}
 }
 
-func getBatch(n int64, pool int64) (res []user) {
-	return nil
+func getBatch(usersNumber int64, pool int64) (res []user) {
+
+	userIds := make(chan int64, usersNumber)
+	results := make(chan user, usersNumber)
+	var i int64
+	var j int64
+	// iterate over channels number
+	for ; i < pool; i++ {
+		go worker(userIds, results)
+	}
+	for ; j < usersNumber; j++ {
+		userIds <- j
+	}
+	close(userIds)
+
+	// collect results into res var
+	var a int64
+	for ; a < usersNumber; a++ {
+		u := <-results
+		res = append(res, u)
+	}
+	return
+}
+
+func worker(users <-chan int64, results chan<- user) {
+	for j := range users {
+		results <- getOne(j)
+	}
 }
